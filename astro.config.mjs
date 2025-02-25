c1bc12752e75d84f218240bc76e2f5dc92dea071
Ruben Tipiani <rtipiani@gmail.com>
2025-02-24 21:27:48 -0500

    Actualización galería

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
+import sitemap from "astro-sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://iquitosexpedition.com",
     output: "server",
     adapter: vercel(),
     vite: {
         plugins: [tailwindcss()],
     },
+    integrations: [sitemap()]
 });
